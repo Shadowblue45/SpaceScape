d82c5f100d2904bbe1b2cd45ff1388679fda652f
yonat <yonat@DESKTOP-R8S15N5>
2017-11-22 20:54:17 -0800

    front end display with broken overlap check

--- SpaceEscape/src/yonathaWins/newYonathanFrontEnd.java
+++ SpaceEscape/src/yonathaWins/newYonathanFrontEnd.java
@@ -8,43 +8,68 @@ package yonathaWins;
  * 5. stop running code 
  */
 public class newYonathanFrontEnd implements TylerSupport {
-	public static int endIntX;
-	public static int endIntY;
-	public static int startIntX;
-	public static int startIntY;
+	public static String endIntX ="";
+	public static String endIntY="";
+	public static String startIntX="";
+	public static String startIntY="";
+	public static int incompleteLine =0;
 	static boolean checkWin = false;
+	public static String[][] board = new String[3][3];
 	public static void main(String[] args) {
-		String[][] board = new String[5][5];
+		
 		for(int x = 0; x<board.length; x++) {
 			for(int y =0; y<board[x].length;y++) {
-				board[x][y]= ". ";
+				board[x][y]= " .";
 			}
 		}
-		while(!checkWin)) {
-			
+		//while(!checkWin) {
+		//String input = in.nextLine();
 		print(board);
-		}
+		System.out.println(startIntX+" "+startIntY+" "+endIntY+" "+endIntY);
+		//}
 	}
 	
 	private static void print(String[][] board) {
 		String currentLine = "";
-		endIntY = (int)(Math.random()*5);
-		endIntX = (int)(Math.random()*5);
-		board[endIntX][endIntY] = "e ";
-		startIntY = (int)(Math.random()*5);
-		startIntX = (int)(Math.random()*5);
-		while(startIntY==endIntY && startIntX==endIntX ) {
-			startIntY = (int)(Math.random()*5);
-			startIntX = (int)(Math.random()*5);
-		}
-		board[startIntX][startIntY] = "s ";
-		for(int y =4; y>-1;y--) {
+		makeGoal((int)(Math.random()*2+1));
+		//board[0][0]=" a";
+		for(int y =board.length-1; y>-1;y--) {
 			for(int x =0; x<board[y].length;x++) {
 				currentLine += board[x][y];	
 			}
 			currentLine += "\n";
 		}
 		System.out.println(currentLine);
+	}
+	private static void makeGoal(int lines) {
+		
+		int endY = (int)(Math.random()*board.length);
+		int endX = (int)(Math.random()*board.length);
+		 
+		int startY = (int)(Math.random()*board.length);
+		int startX = (int)(Math.random()*board.length);
+		
+		//fix these while loops so that they make sure that code isnt printed over
+		while(startY==endY && startX==endX && board[startX][startY].equals(" .")==true) {
+			startY = (int)(Math.random()*board.length);
+			startX = (int)(Math.random()*board.length);
+		}
+		while(startY==endY && startX==endX && board[endX][endY].equals(" .")==true) {
+			endY = (int)(Math.random()*board.length);
+			endX = (int)(Math.random()*board.length);
+		}
+		
+		endIntY += endY;
+		endIntX += endX;
+		startIntX += startX;
+		startIntY += startY;
+		
+		
+		
+		board[endX][endY] = "e"+incompleteLine+" ";
+		board[startX][startY] = "s"+incompleteLine+" ";
+		incompleteLine++;
+		if(lines>0) makeGoal(lines-1);
 	}
 	
 }
