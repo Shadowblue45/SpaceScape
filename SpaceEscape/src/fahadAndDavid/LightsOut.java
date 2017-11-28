136b34074d1fcba0c5c273d3418cc45205b1dd96
Fahadullah Asif <Fahadullah Asif@DESKTOP-8AAJ3UL.home>
2017-11-27 23:25:00 -0500

    made contents

--- SpaceEscape/src/fahadAndDavid/LightsOut.java
+++ SpaceEscape/src/fahadAndDavid/LightsOut.java
@@ -11,14 +11,16 @@ public class LightsOut extends CaveRoom {
 	public LightsOut(String description) {
 		super(description);
 		this.activeDescription = "Here you have to play a game of Lights Out.\nAll of the spaces on the board must be turned into '+' signs.";
-		this.inactiveDescription = "The person you spoke to earlier is standing here.";
+		this.inactiveDescription = "This is where you completed the game of Lights Out.";
 		this.active = true;
 	}
 	public void enter() {
 		super.enter();
+		System.out.println(getDescription());
 		if(active) {
 			FahadFrontend.main(null);
 		}
+		this.active = false;
 	}
 	
 	public String getDescription() {
@@ -27,8 +29,12 @@ public class LightsOut extends CaveRoom {
 		}else {
 			String npcDesc = "";
 				npcDesc = getActiveDescription();
-			return super.getDescription() + "\n" + npcDesc;
+			return npcDesc;
 		}
+	}
+	
+	public String getContents() {
+		return "L";
 	}
 	
 	public boolean isActive() {
